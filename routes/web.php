b0b7a0c7f0427ca5d262e69315916c71b50f8424
Gabriella Khayutin <khay0001@hz.nl>
2024-06-05 11:27:54 +0200

    made last code changes, for the code to be clearer and easier to read

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,9 @@ use App\Http\Controllers\NasaController;
 Route::get('/', [NasaController::class, 'index']);
 Route::get('/daily-pictures', [Dailypicture::class, 'daily']);
 Route::get('/picture/{date}', [Dailypicture::class, 'show'])->name('picture.show');
-
 Route::get('/trigger-500', function () {
     throw new \Exception('This is a simulated server error.');
 });
-//Route::group(['scheme' => 'https'], function () {
-//    // Route::get(...)->name(...);
-//});
 
 
 
